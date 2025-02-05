3d1ba973c0f4eb53dcf140fa2cf83abc6a2157b6
Lionel Melin <53967940+lionelmelin@users.noreply.github.com>
2025-02-05 15:42:59 +0100

    US counties map of unemployment with selector

--- streamlit_app.py
+++ streamlit_app.py
@@ -192,3 +192,30 @@ st.altair_chart(
     .configure_mark(opacity=0.2, color="red"),
     use_container_width=True,
 )
+
+#####################################################################################################################
+
+st.title("US counties unemployment with selector")
+
+from urllib.request import urlopen
+import json
+with urlopen('https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json') as response:
+    counties = json.load(response)
+
+
+df = pd.read_csv("https://raw.githubusercontent.com/plotly/datasets/master/fips-unemp-16.csv",
+                   dtype={"fips": str})
+
+import plotly.express as px
+
+fig = px.choropleth(df, geojson=counties, locations='fips', color='unemp',
+                           color_continuous_scale="Viridis",
+                           range_color=(0, 12),
+                           scope="usa",
+                           labels={'unemp':'unemployment rate'}
+                          )
+fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+
+event = st.plotly_chart(fig, on_select="rerun", selection_mode=["points","box","lasso"])
+
+event
